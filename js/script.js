d4869b3e82fef04f8aef9edd8a6321f7d7f6944c
AntonioScarnera <scarnerantonio07@gmail.com>
2022-04-04 16:57:13 +0200

    conclusione layout

--- js/script.js
+++ js/script.js
@@ -8,21 +8,20 @@ let btn = document.getElementById("btn");
 console.log(selector, btn);
 
 
-function stampareGriglia(){
-    const colNumber = 100;
+function stampareGriglia(numCol, nomeClasse){
     let main = document.getElementById("app");
     main.innerHTML = '';
     let row = document.createElement("div");
     row.setAttribute("class", "row");
-    let cols = creaColonne(colNumber);
+    let cols = creaColonne(numCol, nomeClasse);
     row.innerHTML = cols;
     main.append(row);
 }
-function creaColonne(numerocolonne){
+function creaColonne(numerocolonne, classMod){
     let cols = "";
     for (let i = 1; i <= numerocolonne; i++){
         cols +=`
-        <div class="mycol easy-width">${i}</div>
+        <div class="mycol ${classMod}">${i}</div>
         `;
     }
     return cols;
@@ -37,19 +36,29 @@ btn.addEventListener('click', function(){
 
     switch(flag){
 
-        case "easy": stampareGriglia();
-        let easyCol = document.querySelectorAll(".mycol");
+        case "easy":
+        const easycolNumber = 100;
+        const easyclassMod = "easy-width";
+        stampareGriglia(easycolNumber, easyclassMod);
+        let easyCol = document.getElementsByClassName("mycol");
         console.log(easyCol);
         break;
 
-        case "hard": stampareGriglia();
-        let hardCol = document.querySelectorAll(".mycol");
-        console.log(hardCol);
-        break;
+        case "hard":
+            const hardcolNumber = 81;
+            const hardclassMod = "hard-width";
+            stampareGriglia(hardcolNumber, hardclassMod);
+            let hardCol = document.getElementsByClassName("mycol");
+            console.log(hardCol);
+            break;
         
-        case stampareGriglia();
-        let crazyCol = document.querySelectorAll(".mycol");
-        console.log(crazyCol);
+        case "crazy":
+            const crazycolNumber = 49;
+            const crazyclassMod = "crazy-width";
+            stampareGriglia(crazycolNumber, crazyclassMod);
+            let crazyCol = document.getElementsByClassName("mycol");
+            console.log(crazyCol);
+            break;
 
     }
 
